e247634eefad82d46fc87af94d86ab8e1c0848fb
Afeezope <afeezope1994@gmail.com>
2024-01-07 23:26:05 +0000

    fix code

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-	  task = true;
-	  task2 = false;
+    task = true;    // No 'var' redeclaration, just assignment
+    task2 = false;   // No 'var' redeclaration, just assignment
   }
 
   return [task, task2];
